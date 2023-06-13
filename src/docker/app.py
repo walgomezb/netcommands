ba4431f9fc97963da6749bf900d2df19f9f3235b
wagomez <wagomez@cisco.com>
2023-06-12 18:25:45 -0600

    adding Api

--- src/docker/app.py
+++ src/docker/app.py
@@ -2,14 +2,19 @@ import netmiko
 import sys
 import json
 import os
+import time
+import random
+from datetime import datetime
 
 
 jobInfo = json.loads(sys.argv[1])
+inicio = datetime.now()
+print("Inicio Request=  " + str(inicio))
 print("JobId received: ", jobInfo["id"])
-print("Job Information", sys.argv[1])
-
-print("App started failed", file=sys.stderr)
-
+print("Job Information ", sys.argv[1])
+espera = random.randint(0, 10)
+time.sleep(espera)
+print("TiempoEspera Segs= "+ str(espera))
 
 outPath = "/jobsoutput/"+jobInfo["id"]
 if not os.path.exists(outPath):
@@ -21,4 +26,5 @@ outFile = outPath + "/out.txt"
 f = open(outFile, "w")
 f.write("Resultados de comandos")
 f.close()
-
+final = datetime.now()
+print("Final Request  " + str(final))
---
+++ src/netCommandApi.py
@@ -0,0 +1,57 @@
+from flask import Flask
+import docker
+from docker.types import LogConfig
+import json
+import binascii
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/executeNetDeviceCommands',methods=['GET'])
+def executeNetDeviceCommands():
+    jobId = binascii.b2a_hex(os.urandom(10))
+    jobId = jobId.decode('utf-8')
+
+    IMAGE_NAME = "netcommand"
+    #client = docker.from_env()
+    client = docker.DockerClient(base_url='unix:///Users/wagomez/.docker/run/docker.sock')
+    lc = LogConfig(type=LogConfig.types.JSON, config={
+    'max-size': '1g',
+    })
+    instanceName = "netcommand"+str(jobId)
+
+    jobInfo = {
+        "id": jobId,
+        "routers": [ {"ip": "ip1", "cta": "cta1", "password": "pass1", 
+                    "commands":["show run", "show int bri"]
+                    }
+
+        ]
+    }
+
+    jobInfo = json.dumps(jobInfo)
+
+
+    containerNetcommand = client.containers.run(image=IMAGE_NAME, detach=False,
+                                                                name=instanceName,
+                                                                auto_remove=True,
+                                                                #volumes_from='webstreamLive',
+                                                                #command="streamlink -l info --http-header streamtype=live --http-proxy http://proxyDevOps:8881/ hls://"+urlClient+" best -f -o /webstore/cont"+instance+".ts"
+                                                                log_config = lc,
+                                                                stderr = True,
+                                                                stdout = True,
+                                                                command = "'"+jobInfo+"'",
+                                                                volumes_from='voljobsoutput'
+                                                                )
+
+
+    #containerNetcommand.wait()
+    #print ("termino")
+    #print(containerNetcommand)
+    return containerNetcommand
+
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0", port=8051)
